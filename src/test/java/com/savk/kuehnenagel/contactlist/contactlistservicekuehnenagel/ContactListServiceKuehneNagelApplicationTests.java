68d3fa922016630386bd320b132c76c51c9addad
Anthony Vinay Kumar S <anthonyvinaykumar1992@gmail.com>
2023-01-22 22:17:11 +0200

    Refactor

--- src/test/java/com/savk/kuehnenagel/contactlist/contactlistservicekuehnenagel/ContactListServiceKuehneNagelApplicationTests.java
+++ src/test/java/com/savk/kuehnenagel/contactlist/contactlistservicekuehnenagel/ContactListServiceKuehneNagelApplicationTests.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @ActiveProfiles("h2")
@@ -18,9 +17,6 @@ class ContactListServiceKuehneNagelApplicationTests {
 
     @Autowired
     private ContactService contactService;
-
-    @Autowired
-    private MockMvc mockMvc;
 
     @Test
     void checkWhetherDataIsPersisted () {
--- src/test/java/com/savk/kuehnenagel/contactlist/contactlistservicekuehnenagel/controller/ContactListControllerTests.java
+++ src/test/java/com/savk/kuehnenagel/contactlist/contactlistservicekuehnenagel/controller/ContactListControllerTests.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -21,9 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@ActiveProfiles("h2")
 @AutoConfigureMockMvc
-@TestPropertySource(locations = "classpath:application-h2.properties")
 public class ContactListControllerTests {
 
 	@Autowired
